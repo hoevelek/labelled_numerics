3811bce84febf97699d06251140c9adc43de58c0
Höveler, Katharina <katharina.hoeveler@empa.ch>
2023-12-14 15:26:47 +0100

    added __add__

--- mypackage/__init__.py
+++ mypackage/__init__.py
@@ -1,6 +1,8 @@
 from .geometry import Line, Point
 from .utils import distance
 
+__all__ = ["Line", "Point", "distance"]  # pretends of using it
+
 
 def personal_message():
     print("Hello from John Doe!")
--- mypackage/geometry/__init__.py
+++ mypackage/geometry/__init__.py
@@ -1,1 +1,3 @@
 from .geometry import Line, Point
+
+__all__ = ["Line", "Point"]  # defines API for this package (not considerd as unused)
